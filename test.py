612092a09ddce0d994d8d7bda0cc3f22abdbc317
Yan <yancorrea1995@gmail.com>
2018-06-04 17:01:33 -0300

    travis adicionado

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import pytest
 from principal import multiplicar
 
 def test_multiplicar():
-	assert multiplicar(2,4) == 6
-	assert multiplicar(3,3) == 9
-	assert multiplicar(3,4) == 12
+	assert multiplicar(2,4) == 8
+	assert multiplicar(3,3) == 10
+	assert multiplicar(3,4) == 15
 	assert multiplicar(7,5) == 35
 
 
