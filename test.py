ec13fc026a251370d6ffac99f3080da6420fca5f
Yan <yancorrea1995@gmail.com>
2018-06-04 17:06:03 -0300

    travis adicionado

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from principal import multiplicar
 
 def test_multiplicar():
 	assert multiplicar(2,4) == 8
-	assert multiplicar(3,3) == 10
-	assert multiplicar(3,4) == 15
+	assert multiplicar(3,3) == 9
+	assert multiplicar(3,4) == 12
 	assert multiplicar(7,5) == 35
 
 
